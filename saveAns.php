b676bf1701838a0d81479f8322f185400940e612
yuyuanhey <cook19960626@gmail.com>
2017-02-25 23:54:54 +0800

    check

--- saveAns.php
+++ saveAns.php
@@ -1,5 +1,8 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
+	// header('Access-Control-Allow-Origin: http://test.com');
+	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+	header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
 	// receive the ans that whether it will rain
 	$ans  = $_POST["ifrain"];
 	// $json is now a string
